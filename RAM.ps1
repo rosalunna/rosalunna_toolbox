feb9946e98985f35d22a26fea5d9f2d47ff43e7b
rosalunna <71339547+rosalunna@users.noreply.github.com>
2024-11-05 00:35:24 -0300

    Add files via upload

---
+++ RAM.ps1
@@ -0,0 +1,6 @@
+# Re-run this script hidden using Start-Process if not already hidden
+if ($MyInvocation.WindowHandle -ne [System.IntPtr]::Zero) {
+    Start-Process powershell -ArgumentList "-WindowStyle Hidden -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+Rammap -E0 -Et -Ew
