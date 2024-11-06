c2b117e2cd5c14727840a2e3d0b203081c62d576
rosalunna <71339547+rosalunna@users.noreply.github.com>
2024-11-05 23:09:58 -0300

    Add files via upload

--- defender.bat
+++ defender.bat
@@ -1,3 +1,5 @@
+@echo off
+powershell -Command
 $remove_appx = @("SecHealthUI"); $provisioned = get-appxprovisionedpackage -online; $appxpackage = get-appxpackage -allusers; $eol = @()
 $store = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore'
 $users = @('S-1-5-18'); if (test-path $store) {$users += $((dir $store -ea 0 |where {$_ -like '*S-1-5-21*'}).PSChildName)}
