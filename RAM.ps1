97b71e13f501d787e2bc4070153a4ad6515afddd
rosalunna <71339547+rosalunna@users.noreply.github.com>
2024-11-05 22:57:48 -0300

    Delete RAM.ps1

--- RAM.ps1
+++
@@ -1,6 +0,0 @@
-# Re-run this script hidden using Start-Process if not already hidden
-if ($MyInvocation.WindowHandle -ne [System.IntPtr]::Zero) {
-    Start-Process powershell -ArgumentList "-WindowStyle Hidden -File `"$PSCommandPath`"" -Verb RunAs
-    exit
-}
-Rammap -E0 -Et -Ew
