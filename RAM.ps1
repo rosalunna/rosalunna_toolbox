59b653820dd3fb71723ff122c6af551f3d4e7ef0
rosalunna <71339547+rosalunna@users.noreply.github.com>
2024-11-06 23:52:33 -0300

    Update RAM.ps1

--- RAM.ps1
+++ RAM.ps1
@@ -1,6 +1,8 @@
-# Re-run this script hidden using Start-Process if not already hidden
-if ($MyInvocation.WindowHandle -ne [System.IntPtr]::Zero) {
+# Run once with elevated privileges if needed
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell -ArgumentList "-WindowStyle Hidden -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
+
+# Run the RAM cleanup command once
 Rammap -E0 -Et
